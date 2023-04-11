0628501ba247a3dfb80096f65ed63af993c86174
hscott13 <hscott13@hotmail.co.uk>
2023-04-11 18:21:12 +0100

    Fixed time bug

--- src/DateFormat.js
+++ src/DateFormat.js
@@ -1,6 +1,9 @@
 function FormatDate(props) {
   let hours = props.date.getHours();
   let minutes = props.date.getMinutes();
+  if (minutes.length == 1) {
+    minutes = "0" + minutes;
+  }
   let day = props.date.getDate();
   let month = props.date.getMonth() + 1;
   let year = props.date.getFullYear();
